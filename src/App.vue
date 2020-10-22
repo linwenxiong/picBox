619ad874993c546a8594395f66ea4140f7487d0b
林文雄 <EX_110904150@hdjt.hdad.local>
2020-10-22 18:33:36 +0800

    代码提交

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+<style>
+  img{
+    display: block;
+    width: 100%;
+  }
+  div, section, html, body{
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+</style>
---
+++ src/view/home.vue
@@ -0,0 +1,39 @@
+<template>
+    <section class="pic">
+      <div class="pic-items">
+        <img src="https://hdbbmp-dev.oss-cn-shenzhen.aliyuncs.com/hdb/2020/09/10/d5ddfaef6f43405895b3985979f00178" large="https://hdbbmp-dev.oss-cn-shenzhen.aliyuncs.com/hdb/2020/09/10/d5ddfaef6f43405895b3985979f00178" preview="2" preview-text="缩略图与大图模式">
+      </div>
+      <div class="pic-items">
+        <img src="https://hdbbmp-dev.oss-cn-shenzhen.aliyuncs.com/hdb/2020/09/04/b566b6c6ba9d4608b42749c541ff6bca" large="https://hdbbmp-dev.oss-cn-shenzhen.aliyuncs.com/hdb/2020/09/04/b566b6c6ba9d4608b42749c541ff6bca" preview="2" preview-text="缩略图与大图模式">
+      </div>
+      <div class="pic-items">
+        <img src="https://hdbbmp-dev.oss-cn-shenzhen.aliyuncs.com/hdb/2020/09/04/b566b6c6ba9d4608b42749c541ff6bca" large="https://hdbbmp-dev.oss-cn-shenzhen.aliyuncs.com/hdb/2020/09/04/b566b6c6ba9d4608b42749c541ff6bca" preview="2" preview-text="缩略图与大图模式">
+      </div>
+      <div class="pic-items">
+        <img src="https://hdbbmp-dev.oss-cn-shenzhen.aliyuncs.com/2018/1_1_1/1d137d4a-b1cc-4359-b61e-e6e2742aae8d.jpg" large="https://hdbbmp-dev.oss-cn-shenzhen.aliyuncs.com/2018/1_1_1/1d137d4a-b1cc-4359-b61e-e6e2742aae8d.jpg" preview="2" preview-text="缩略图与大图模式">
+      </div>
+    </section>
+</template>
+
+<script>
+export default {
+  name: 'home'
+}
+</script>
+
+<style scoped>
+  .pic{
+    display: flex;
+    justify-content: space-between;
+    flex-direction: row;
+    flex-wrap: wrap;
+    padding: 5px;
+  }
+  .pic-items{
+    width: 50%;
+    padding: 5px;
+  }
+  img{
+    border-radius: 10px;
+  }
+</style>
